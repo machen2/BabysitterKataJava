80c5991e4ffe53760912e1b42bfbf1bcca599bb2
Tori Machen <tmachen@Tori-Machen.local>
2018-02-08 12:07:56 -0600

    handles invalid start time

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,7 +1,23 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class InputValidator {
-    private int startTime = 5;
+    private int inputStartTime;
+
+    public InputValidator(int inputStartTime) {
+        this.inputStartTime = validateStartTimeInput(inputStartTime);
+    }
+
+    private int validateStartTimeInput(int inputStartTime) {
+        if (inputStartTime < 17) {
+            return 17;
+        }
+        return inputStartTime;
+    }
 
     public int getStartTime() {
-        return startTime;
+        return inputStartTime;
     }
+
 }
--- src/test/java/InputValidatorTest.java
+++ src/test/java/InputValidatorTest.java
@@ -1,12 +1,25 @@
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 public class InputValidatorTest {
 
     @Test
     public void given_UserEntersAValidStartTime_Then_ThatTimeIsSet(){
-        InputValidator inputValidator = new InputValidator();
-        assertEquals(5, inputValidator.getStartTime());
+        int expectedStartTime = 17;
+        InputValidator inputValidator = new InputValidator(expectedStartTime);
+        assertEquals(expectedStartTime, inputValidator.getStartTime());
+    }
+
+    @Test
+    public void given_UserEntersInvalidStartTime_Then_StartTimeIsSetTo5ByDefault() {
+        int expectedStartTime = 17;
+        int invalidStartTime = 2;
+        InputValidator inputValidator = new InputValidator(invalidStartTime);
+        assertEquals(expectedStartTime, inputValidator.getStartTime());
     }
 }
