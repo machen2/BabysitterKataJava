b4987a99275d360c6c4a4683138d41a876cae590
Tori Machen <tmachen@Tori-Machen.local>
2018-02-12 13:23:57 -0600

    end time is before or at 4am

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,23 +1,34 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class InputValidator {
     private int inputStartTime;
+    private int inputEndTime;
 
-    public InputValidator(int inputStartTime) {
+    public InputValidator(int inputStartTime, int inputEndTime) {
         this.inputStartTime = validateStartTimeInput(inputStartTime);
+        this.inputEndTime = validateEndTimeInput(inputEndTime);
+    }
+
+    private int validateEndTimeInput(int inputEndTime) {
+        if (inputEndTime <= 4) {
+            return inputEndTime;
+        }
+        return -1;
     }
 
     private int validateStartTimeInput(int inputStartTime) {
         if (inputStartTime < 17) {
             return 17;
         }
+
         return inputStartTime;
     }
 
+
     public int getStartTime() {
         return inputStartTime;
+    }
+
+    public int getEndTime() {
+        return inputEndTime;
     }
 
 }
--- src/test/java/InputValidatorTest.java
+++ src/test/java/InputValidatorTest.java
@@ -1,9 +1,5 @@
 import org.junit.Test;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import static org.junit.Assert.assertEquals;
 
 public class InputValidatorTest {
@@ -11,15 +7,33 @@ public class InputValidatorTest {
     @Test
     public void given_UserEntersAValidStartTime_Then_ThatTimeIsSet(){
         int expectedStartTime = 17;
-        InputValidator inputValidator = new InputValidator(expectedStartTime);
+        int endTime = 2;
+        InputValidator inputValidator = new InputValidator(expectedStartTime, endTime);
         assertEquals(expectedStartTime, inputValidator.getStartTime());
     }
 
     @Test
-    public void given_UserEntersInvalidStartTime_Then_StartTimeIsSetTo5ByDefault() {
+    public void given_UserEntersInvalidStartTime_Then_StartTimeIsSetTo17ByDefault() {
         int expectedStartTime = 17;
         int invalidStartTime = 2;
-        InputValidator inputValidator = new InputValidator(invalidStartTime);
+        int endTime = 2;
+        InputValidator inputValidator = new InputValidator(invalidStartTime, endTime);
         assertEquals(expectedStartTime, inputValidator.getStartTime());
+    }
+
+    @Test
+     public void given_UserEntersValidEndTime_Then_EndTimeIsSet() {
+        int expectedEndTime = 4;
+        int startTime = 17;
+        InputValidator inputValidator = new InputValidator(startTime, expectedEndTime);
+        assertEquals(expectedEndTime, inputValidator.getEndTime());
+    }
+
+    @Test
+    public void given_UserEntersEndTimeBefore4_Then_EndTimeIsSet() {
+        int expectedEndTime = 3;
+        int startTime = 17;
+        InputValidator inputValidator = new InputValidator(startTime, expectedEndTime);
+        assertEquals(expectedEndTime, inputValidator.getEndTime());
     }
 }
